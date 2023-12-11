2114982685728721408e464c8b2b077b68d68a61
Aasesi <mikolajwilczynnski@gmail.com>
2023-12-11 18:56:58 +0100

    Comments for application

--- MedicalDatabase.py
+++ MedicalDatabase.py
@@ -1,15 +1,26 @@
 class MedicalDatabase:
     def delete(self):
+        # Just normal delete function
         pass
 
     def insert(self):
+        # Insert patient data. Two cases(optional btw we can just add entire row) one for inserting entire row and one
+        # for updating/insert (maybe adding missing data to some patient)
         pass
 
     def interpret_data(self):
+        # I do not remember why we have this
         pass
 
     def correct_data(self):
+        # Correct missing value if any. I think we can make it initialized by click of the button.
         pass
 
     def load_data(self):
+        # Maybe at initialization it checks whether the data.csv(or some other name of database exists). if not write
+        # something to console?
+        pass
+
+    def save_data(self):
+        # Saves data to file. Something should be in parameters
         pass
